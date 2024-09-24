9509b47ce8d2c55a266e48c7e1fe46c6868c5364
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-09-24 19:28:44 -0300

    post

--- 01-Pratica/pratica1/src/components/Post/Post.jsx
+++ 01-Pratica/pratica1/src/components/Post/Post.jsx
@@ -1,8 +1,35 @@
-export function Post(props) {
+import styles from "./styles.module.css";
+
+export function Post() {
   return (
-    <div>
-      <h2>{props.author}</h2>
-      <p>{props.content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://gravatar.com/avatar/0ae5d5a8047a9915e940bb2010ec803b?s=150&d=mp&r=x"
+          />
+          <div className={styles.autorInfo}>
+            <strong>Jao Bao</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time dateTime="2024-05-11 08:15:35">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Lorem, ipsum.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi,
+          sapiente?
+        </p>
+        <p>
+          <a href="">Lorem, ipsum dolor.</a>
+        </p>
+        <p>
+          <a href="">Lorem, ipsum.</a>
+        </p>
+      </div>
+    </article>
   );
 }

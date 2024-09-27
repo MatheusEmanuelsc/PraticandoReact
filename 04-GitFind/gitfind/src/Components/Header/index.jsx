4124d4d0405a81c38cff7d6b1ea1a6666d53a1c2
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-09-27 09:04:38 -0300

    header

---
+++ 04-GitFind/gitfind/src/Components/Header/index.jsx
@@ -0,0 +1,9 @@
+import "./header.module.css";
+
+export function Header() {
+  return (
+    <header>
+      <h1>GitFind</h1>
+    </header>
+  );
+}
--- 04-GitFind/gitfind/src/Pages/Home/index.jsx
+++ 04-GitFind/gitfind/src/Pages/Home/index.jsx
@@ -1,11 +1,17 @@
-
-
+import "../../global.css";
+import styles from "./home.module.css";
+import { Header } from "../../Components/Header";
+import logo from "../../assets/logo.png";
 function App() {
- 
   return (
-    
-     
-  )
+    <>
+      <Header />
+      <main className={styles.conteudo}>
+        <img className={styles.logo} src={logo} alt="logo background" />
+        <section className={styles.info}></section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
